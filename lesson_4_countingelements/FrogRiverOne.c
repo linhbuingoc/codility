8d92a1609668833214fd14879a01b2f9e6fa6563
buingoclinh.hut@gmail.com <buingoclinh.hut@gmail.com>
2017-05-03 20:52:32 +0700

    Add solution to FrogRiverOne problem.

---
+++ lesson_4_countingelements/FrogRiverOne.c
@@ -0,0 +1,20 @@
+int FrogRiverOne(int X, int A[], int N) 
+{
+    int *map = calloc(1+X, sizeof(int));
+    int i, step;
+    for (step = 0, i = 0; i<N; i++)
+    {
+        if (map[A[i]] == 0)
+        {
+            /* Mark this one as available */
+            map[A[i]] = 1;
+            step++;
+            /* The whole road is available now */
+            if (step == X)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
