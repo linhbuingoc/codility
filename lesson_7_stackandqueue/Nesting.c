968e33a7267ab4fbf5f196e80b58923b9f0bb0f2
buingoclinh.hut@gmail.com <buingoclinh.hut@gmail.com>
2017-05-08 21:22:00 +0700

    Add solution for Nesting problem

---
+++ lesson_7_stackandqueue/Nesting.c
@@ -0,0 +1,24 @@
+int Nesting(char *S)
+{
+    int     top;
+    for (top = 0; *S != '\0' ; S++)
+    {
+        if (*S == '(')
+        {
+            ++top;
+        }
+        else
+        {
+            /* Empty stack */
+            if (top == 0)
+            {
+                /* Make the stack appears to be not empty so the function returns false */
+                top = 1;
+                break;
+            }
+            --top;
+        }
+    } 
+
+    return (top == 0);
+}
